998a1cb4a5a71d9169cf59e440af9df5934f9b7e
UnlockGSM <126637558+UnlockGSMO@users.noreply.github.com>
2023-06-22 12:17:26 -0400

    Agregados los paquetes de bootstrap

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 let express = require('express'); // Importo Express para el servidor HTTP
 let app = express(); // Hago una instancia de este para servir al cliente
 let dotenv = require('dotenv'); // Importo dotenv para mantener privada las configuraciones del servidor
+let path = require('path'); // Importo path para usarlo en la carga de archivos 
 
 dotenv.config(); // Cargo las configuraciones explicitas en el archivo .env en las variables de entorno (process.env)
 
 let cors = require('cors'); // Permitir el acceso de origen cruzado
 
-let views = __dirname + '/views/' // Directorio de las views o paginas del servidor
+let views = path.join(__dirname, '/views/') // Directorio de las views o paginas del servidor
 
 app.use(cors({optionsSuccessStatus: 200})); // Hago uso del middleware cors y establezco su opcion 'optionsSuccessStatus' en 200 para compatibilidad en los naveres
-app.use(express.static(__dirname + '/public/')) // Definir la ruta de archivos estaticos publicos del servidor
-
+app.use(express.static(path.join(__dirname,'/public'))) // Definir la ruta de archivos estaticos publicos del servidor
+app.use('/bootstrap/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/'))); // Cargar los componentes de bootstrap (CSS)
+app.use('/bootstrap/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js/'))); // Cargar los componentes de bootstrap (JavaScript)
 
 app.get('/', (req, res) => {    // Sirviendo el root con el archivo index.html
         res.sendFile(views + 'index.html'); // Enviar el archivo index.html al cliente 
