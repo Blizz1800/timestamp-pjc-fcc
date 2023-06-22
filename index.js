56f62f1c235dd3e72944db92269077dbf9117363
UnlockGSM <126637558+UnlockGSMO@users.noreply.github.com>
2023-06-22 11:06:24 -0400

    Comentado todo el code

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let express = require('express');
-let app = express();
-let dotenv = require('dotenv');
+let express = require('express'); // Importo Express para el servidor HTTP
+let app = express(); // Hago una instancia de este para servir al cliente
+let dotenv = require('dotenv'); // Importo dotenv para mantener privada las configuraciones del servidor
 
-let config = dotenv.config();
+dotenv.config(); // Cargo las configuraciones explicitas en el archivo .env en las variables de entorno (process.env)
+
+let cors = require('cors'); // Permitir el acceso de origen cruzado
+
+let views = __dirname + '/views/' // Directorio de las views o paginas del servidor
+
+app.use(cors({optionsSuccessStatus: 200})); // Hago uso del middleware cors y establezco su opcion 'optionsSuccessStatus' en 200 para compatibilidad en los naveres
+app.use(express.static(__dirname + '/public/')) // Definir la ruta de archivos estaticos publicos del servidor
 
 
-let cors = require('cors');
-
-app.use(cors({optionsSuccessStatus: 200}));
-
-views = __dirname + '/views/'
-
-app.get('/', (req, res) => {
-        res.sendFile(views + 'index.html');
+app.get('/', (req, res) => {    // Sirviendo el root con el archivo index.html
+        res.sendFile(views + 'index.html'); // Enviar el archivo index.html al cliente 
     });
 
-app.listen(process.env.PUERTO, () => {
-    console.log('listening on port ' + process.env.PUERTO);
+app.listen(process.env.PUERTO, () => { // Pone al servidor en escucha en el puerto especificado
+    console.log('listening on port ' + process.env.PUERTO); // Hace un log al completar la escucha
 });
